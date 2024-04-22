e945ec0e1192238d55423b7031d79267fe84a7b2
最好真好 <8257167+boyLong@user.noreply.gitee.com>
2024-04-22 19:44:06 +0800

    1

--- start_server.py
+++ start_server.py
@@ -9,6 +9,6 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 print(os.path.dirname(os.path.abspath(__file__)))
 subprocess.Popen("node encrypt/temu_api.js", shell=True)
 
-subprocess.Popen(f"uvicorn captcha.api:app --reload --port {server_config['captcha_port']}", shell=True)
+subprocess.Popen(f"uvicorn captcha.api:app --port {server_config['captcha_port']}", shell=True)
 
 uvicorn.run(app, host="0.0.0.0", port=server_config['server_port'],reload=True)
