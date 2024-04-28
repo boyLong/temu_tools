21f9a742854923c78e50167b981ba3650b0d3d3b
最好真好 <8257167+boyLong@user.noreply.gitee.com>
2024-04-28 17:09:22 +0800

    1

--- kill-server.sh
+++ kill-server.sh
@@ -1,3 +1,3 @@
 kill -9 $(ps -ef | pgrep captcha.api:app)
-kill -9 $(ps -ef | pgrep node encrypt/temu_api.js)
-kill -9 $(ps -ef | pgrep node start_server.py)
+kill -9 $(ps -ef | pgrep encrypt/temu_api.js)
+kill -9 $(ps -ef | pgrep start_server.py)
