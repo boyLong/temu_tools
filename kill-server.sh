a3b232820c7c7305f49d399e64552eb815895cf4
最好真好 <8257167+boyLong@user.noreply.gitee.com>
2024-04-28 17:19:12 +0800

    1

--- kill-server.sh
+++ kill-server.sh
@@ -1,22 +1,20 @@
-
-pro_id=`ps -ef | grep captcha.api:app | grep -v grep | awk '{print $2}'`
+pro_id=$(ps -ef | grep captcha.api:app | grep -v grep | awk '{print $2}')
 # 判断进程id是否为空，若不为空，则杀掉进程
-if [[ $pro_id != ""]];then
-	echo "kill the " "captcha.api:app" "process_id is " $pro_id
-	kill -9 $pro_id
+if [[ $pro_id != "" ]]; then
+    echo "kill the captcha.api:app process_id is $pro_id"
+    kill -9 $pro_id
 fi
 
-
-pro_id=`ps -ef | grep encrypt/temu_api.js | grep -v grep | awk '{print $2}'`
+pro_id=$(ps -ef | grep encrypt/temu_api.js | grep -v grep | awk '{print $2}')
 # 判断进程id是否为空，若不为空，则杀掉进程
-if [[ $pro_id != ""]];then
-	echo "kill the " "<temu_api>" "process_id is " $pro_id
-	kill -9 $pro_id
+if [[ $pro_id != "" ]]; then
+    echo "kill the <temu_api> process_id is $pro_id"
+    kill -9 $pro_id
 fi
 
-pro_id=`ps -ef | grep start_server.py | grep -v grep | awk '{print $2}'`
+pro_id=$(ps -ef | grep start_server.py | grep -v grep | awk '{print $2}')
 # 判断进程id是否为空，若不为空，则杀掉进程
-if [[ $pro_id != ""]];then
-	echo "kill the " "<start_server>" "process_id is " $pro_id
-	kill -9 $pro_id
+if [[ $pro_id != "" ]]; then
+    echo "kill the <start_server> process_id is $pro_id"
+    kill -9 $pro_id
 fi
