1c10628e4cf09d756c3b5d28d3382a7f5c02bbd1
最好真好 <8257167+boyLong@user.noreply.gitee.com>
2024-04-28 17:15:02 +0800

    1

--- kill-server.sh
+++ kill-server.sh
@@ -1,3 +1,22 @@
-kill -9 $(ps -ef | pgrep captcha.api:app)
-kill -9 $(ps -ef | pgrep encrypt/temu_api.js)
-kill -9 $(ps -ef | pgrep start_server.py)
+
+pro_id=`ps -ef | grep captcha.api:app | grep -v grep | awk {print $2}`
+# 判断进程id是否为空，若不为空，则杀掉进程
+if [[ $pro_id != ""]];then
+	echo "kill the " "captcha.api:app" "process_id is " $pro_id
+	kill -9 $pro_id
+fi
+
+
+pro_id=`ps -ef | grep encrypt/temu_api.js | grep -v grep | awk {print $2}`
+# 判断进程id是否为空，若不为空，则杀掉进程
+if [[ $pro_id != ""]];then
+	echo "kill the " "<temu_api>" "process_id is " $pro_id
+	kill -9 $pro_id
+fi
+
+pro_id=`ps -ef | grep start_server.py | grep -v grep | awk {print $2}`
+# 判断进程id是否为空，若不为空，则杀掉进程
+if [[ $pro_id != ""]];then
+	echo "kill the " "<start_server>" "process_id is " $pro_id
+	kill -9 $pro_id
+fi
