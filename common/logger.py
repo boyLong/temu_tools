7e07681bbe3e8f0d2c50d7d80dc69750a0eac34b
最好真好 <8257167+boyLong@user.noreply.gitee.com>
2024-04-28 17:00:44 +0800

    1

--- common/logger.py
+++ common/logger.py
@@ -1,9 +1,11 @@
 from loguru import logger
 
 logger.add(
-    sink='temu_login.log',
+    sink='temu.log',
     level='INFO',
     encoding='utf-8',
     enqueue=True,
-    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {module} | {file} | {message}"
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {module} | {file} | {message}",
+    rotation=1,
+    retention=7
 )
